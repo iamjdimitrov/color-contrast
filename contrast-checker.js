2a73a0df2801154795bfc1cf6a447c92dc662194
Jordan Dimitrov <jordan.h.dimitrov@gmail.com>
2019-06-12 12:17:28 +0200

    Update contrast-checker.js

--- contrast-checker.js
+++ contrast-checker.js
@@ -1,5 +1,7 @@
 function calculateLuminance(color) {
-  var triplets = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+  color = color.replace("#", "").trim();
+	/^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.test(color) ? color : /^([a-f\d]{1})([a-f\d]{1})([a-f\d]{1})$/i.test(color) ? color = color.replace(/^([a-f\d]{1})([a-f\d]{1})([a-f\d]{1})/i, "$1$1$2$2$3$3") : null;
+  var triplets = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
   for (var rgb = [], weighted = [], t = 1; t <= 3; t++) {
     rgb[t] = parseInt(triplets[t], 16) / 255;
     weighted[t] = rgb[t] <= 0.03928 ? rgb[t] / 12.92 : Math.pow((rgb[t] + 0.055) / 1.055, 2.4);
